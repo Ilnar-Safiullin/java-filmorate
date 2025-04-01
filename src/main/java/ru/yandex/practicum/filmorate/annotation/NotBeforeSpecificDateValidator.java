179c39e331dcbc520338f0d9a7063b18b27bba95
Ilnar <safiullinko@yandex.ru>
2025-04-02 01:43:45 +0300

    первые правки

--- src/main/java/ru/yandex/practicum/filmorate/annotation/NotBeforeSpecificDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/NotBeforeSpecificDateValidator.java
@@ -6,10 +6,10 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class NotBeforeSpecificDateValidator implements ConstraintValidator<NotBeforeSpecificDate, LocalDate> {
-    private final LocalDate MIN_DATE = LocalDate.of(1895, 12, 28);
+    private final LocalDate minDate = LocalDate.of(1895, 12, 28); //чек стайл не пропускает MIN_DATE
 
     @Override
     public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext context) {
-        return releaseDate != null && !releaseDate.isBefore(MIN_DATE);
+        return releaseDate != null && !releaseDate.isBefore(minDate);
     }
 }
