2bfb9aa9c79f5335d9ebbd9bb9df51015fda46cb
Ilnar <safiullinko@yandex.ru>
2025-05-18 19:57:17 +0300

    исправить стораджи, разбить стораджи, исправить сервисы, добавить тесты, убрать циклы

--- src/main/java/ru/yandex/practicum/filmorate/storage/likes/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/likes/LikeDbStorage.java
@@ -17,14 +17,14 @@ public class LikeDbStorage {
     private static final String INSERT_ADD_LIKE_FILM = "INSERT INTO likes(film_id, user_id) VALUES (?, ?)";
     private static final String DELETE_LIKE_FILM = "DELETE FROM likes WHERE film_id = ? AND user_id = ?";
     private static final String GET_POPULAR_FILMS = """
-            SELECT f.film_id, f.name, f.description, f.release_date, f.duration, f.mpa_id, m.name AS mpa_name, 
+            SELECT f.film_id, f.name, f.description, f.release_date, f.duration, f.mpa_id, m.name AS mpa_name,
             g.genre_id, g.name AS genreName, fg.genre_id AS chekgenre,
             COUNT(l.user_id) AS likes_count
             FROM
                 films f
             LEFT JOIN film_genre fg ON f.film_id = fg.film_id
             LEFT JOIN genres g ON fg.genre_id = g.genre_id
-            LEFT JOIN mpa_rating m ON f.mpa_id = m.mpa_id      
+            LEFT JOIN mpa_rating m ON f.mpa_id = m.mpa_id
             LEFT JOIN
                 likes l ON f.film_id = l.film_id
             GROUP BY
